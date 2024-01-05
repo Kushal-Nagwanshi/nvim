c356d14a3b15243ed23f68b8046a1b23bcdd09f3
Kushal Nagwanshi <kushalnagwanshicloud@gmail.com>
2024-01-06 03:13:52 +0530

    remove: obsolete comment

--- lua/davinci/plugins/trouble.lua
+++ lua/davinci/plugins/trouble.lua
@@ -11,9 +11,6 @@ function M.config()
 
     require("trouble").setup {
         icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
     }
 end
 
